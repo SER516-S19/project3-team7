f1635ceaefc0a17557c61c104bb60b3f58f77a87
Nikhila6 <saini.nikhila@gmail.com>
2019-03-30 20:18:17 -0700

    Created static page using JavaFX which allows professor to create quiz with Multiple Choice Questions

--- professor/src/ProfessorApplication.java
+++ professor/src/ProfessorApplication.java
@@ -9,8 +9,10 @@ public class ProfessorApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/professor.fxml"));
+        Parent root1 = FXMLLoader.load(getClass().getResource("view/CreateQuiz.fxml"));
         primaryStage.setTitle("Hello Professor");
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root1, 800, 500));
         primaryStage.show();
     }
 
