ef462681c63a22703e4600f6f0637e706b6694e0
Jahnvi Rai <jrai4@asu.edu>
2019-03-31 22:45:46 -0700

    Creating a list view to list all the quizzes created by professor

--- student/src/controller/StudentController.java
+++ student/src/controller/StudentController.java
@@ -1,4 +1,31 @@
 package controller;
 
-public class StudentController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentController implements Initializable {
+
+    @FXML
+    private ListView<String> quizList;
+
+    private ObservableList quizNames =  FXCollections.observableArrayList();
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       getQuizNames();
+    }
+
+    public void getQuizNames(){
+        quizNames.add("Quiz1");
+        quizNames.add("Quiz2");
+        quizNames.add("Quiz3");
+        quizList.getItems().addAll(quizNames);
+    }
 }
