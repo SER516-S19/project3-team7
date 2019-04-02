66f2c93e20f005c53c470758163546b36a988e18
Bhawana Prasad <bprasad1@asu.edu>
2019-04-02 02:56:10 -0700

    Adding the model classes for quiz and questions

--- professor/src/controller/QuizController.java
+++ professor/src/controller/QuizController.java
@@ -1,5 +1,26 @@
 package controller;
 
-public class QuizController{
-	
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QuizController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void addQuiz(){
+
+    }
+
+    public void addQuestion(){
+        System.out.println("Add question");
+    }
+
+    public void saveAndExit(){
+        System.out.println("Save and exit");
+    }
 }
