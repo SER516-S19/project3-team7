05e2d66df5cdf0c54a303b3fec4e4c7b9c97667d
Yuvan <yuvanpradeep594@gmail.com>
2019-04-02 15:23:52 -0700

    Minor change in parameter passing.

--- student/src/controller/QuestionAnswerController.java
+++ student/src/controller/QuestionAnswerController.java
@@ -2,22 +2,28 @@ package controller;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import model.QuizDetails;
-
 import java.io.File;
 import java.io.IOException;
+
+/**
+ * Class to render Question and Answer from the Json file
+ */
 public class QuestionAnswerController {
 
-    public void fetchQuizDetails(){
+    public void fetchQuizDetails(String selectedQuiz){
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            QuizDetails quiz = objectMapper.readValue(new File("quiz/quiz1.json"), QuizDetails.class);
+            QuizDetails quiz = objectMapper.readValue(new File("quiz/"+selectedQuiz+".json"), QuizDetails.class);
             System.out.println(quiz);
         }
         catch (JsonParseException e) { e.printStackTrace();}
         catch (JsonMappingException e) { e.printStackTrace(); }
         catch (IOException e) { e.printStackTrace(); }
-
 
     }
 }
