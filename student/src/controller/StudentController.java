9a54553d1f1a1116c8df3fa6a51c988608500861
Jahnvi Rai <jrai4@asu.edu>
2019-04-03 02:16:27 -0700

    Load the Quiz Screen on selecting the quiz

--- student/src/controller/StudentController.java
+++ student/src/controller/StudentController.java
@@ -5,9 +5,15 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -28,6 +34,7 @@ public class StudentController implements Initializable {
        getQuizNames(); listViewEventListener();
     }
 
+
     public void getQuizNames(){
         quizNames.add("Quiz1");
         quizNames.add("Quiz2");
@@ -36,14 +43,30 @@ public class StudentController implements Initializable {
     }
 
 
-    public void listViewEventListener() {
+    public void listViewEventListener()  {
         quizList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String selectedQuiz) {
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String selectedQuiz)  {
                 //System.out.println("Selected item: " + selectedQuiz);
+                try {
+                    loadQuizWindow();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
                 questionAnswerObj.fetchQuizDetails(selectedQuiz);
             }
         });
+    }
+
+    private void loadQuizWindow() throws IOException {
+        Stage quizWindow = (Stage) quizList.getScene().getWindow();
+        Parent root  = FXMLLoader.load(getClass().getResource("/view/showQuiz.fxml"));
+        Scene scene = new Scene(root);
+        quizWindow.setScene(scene);
+        quizWindow.show();
+        quizWindow.setResizable(false);
+
+
     }
 
 }
