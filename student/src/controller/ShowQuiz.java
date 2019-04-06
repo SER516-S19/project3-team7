634de526c1d4d0a781a56a724d56f7d1bec48fbb
Lakshmi Kala <raghavakannikanti@Lakshmi-MacBook-Air.local>
2019-04-06 16:11:18 -0700

    Implemented next button functionality and display questions from json file.

--- student/src/controller/ShowQuiz.java
+++ student/src/controller/ShowQuiz.java
@@ -1,88 +1,116 @@
 package controller;
+
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import model.Question;
 import model.QuizDetails;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class to render Question and Answer from the Json file
  */
 public class ShowQuiz {
 
-    @FXML
-    public Label quizName;
+	@FXML
+	public Label QuizName;
 
-    public Text questionTitle;
-    public Text questionNumber;
+	private List<Question> questions = new ArrayList<>();
 
-    public RadioButton option1;
-    public RadioButton option2;
-    public RadioButton option3;
-    public RadioButton option4;
+	@FXML
+	private Text questionTitle;
 
+	@FXML
+	private Text questionNumber;
 
-    String questionTitleText;
+	@FXML
+	public RadioButton option1;
 
-    String option1Text;
-    String option2Text;
-    String option3Text;
-    String option4Text;
+	@FXML
+	public RadioButton option2;
 
-    public QuizDetails quiz;
+	@FXML
+	public RadioButton option3;
 
-    public void fetchQuizDetails(String selectedQuiz){
-        int _INIT = 0;
+	@FXML
+	public RadioButton option4;
 
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            quiz = objectMapper.readValue(new File("quiz/"+selectedQuiz+".json"), QuizDetails.class);
-            System.out.println(quiz);
-            loadQuestionAnswer(_INIT);
-        }
-        catch (JsonParseException e) { e.printStackTrace();}
-        catch (JsonMappingException e) { e.printStackTrace(); }
-        catch (IOException e) { e.printStackTrace(); }
-    }
+	@FXML
+	public Button nextButton;
 
-    public void endQuiz(){}
+	private int currentQuestionNumber = 1;
 
-    public void loadQuestionAnswer(int index) {
-        questionTitleText = quiz.getQuestions().get(index).getTitle();
-        option1Text = quiz.getQuestions().get(index).getOptions().get(0);
-        option2Text = quiz.getQuestions().get(index).getOptions().get(1);
-        option3Text = quiz.getQuestions().get(index).getOptions().get(2);
-        option4Text = quiz.getQuestions().get(index).getOptions().get(3);
+	public void fetchQuizDetails(String selectedQuiz) {
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			QuizDetails quiz = objectMapper.readValue(new File("quiz/" + selectedQuiz + ".json"), QuizDetails.class);
+			questions.addAll(quiz.getQuestions());
+			setQuestions();
+			System.out.println(quiz);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-        try {
-            questionNumber.setText(Integer.toString(index + 1));
-            questionTitle.setText(questionTitleText);
-            option1.setText(option1Text);
-            option2.setText(option2Text);
-            option3.setText(option3Text);
-            option4.setText(option4Text);
-        }catch (NullPointerException ex) {
-            ex.printStackTrace();
-        }
-    }
+	public void endQuiz() {
+	}
 
-   public void showNextQuestion(){ }
+	public void showNextQuestion() {
+		int quizSize = questions.size();
 
-    public void submitQuiz(){
-        //TODO add code for submit quiz
-    }
+		if (currentQuestionNumber <= quizSize) {
+			setQuestions();
+		}
 
-    public void setQuizTitleLabel(String selectedQuiz) {
+		if (currentQuestionNumber - 1 == quizSize) {
+			nextButton.setText("Submit");
+		}
+	}
 
-        quizName.setText(selectedQuiz);
-    }
+	public void setQuizTitleLabel(String selectedQuiz) {
+
+		QuizName.setText(selectedQuiz);
+	}
+
+	public void setQuestions() {
+		Question question = questions.get(currentQuestionNumber - 1);
+		questionNumber.setText(currentQuestionNumber + ")");
+		questionTitle.setText(question.getTitle());
+		currentQuestionNumber = currentQuestionNumber + 1;
+		List<String> options = question.getOptions();
+		ToggleGroup group = new ToggleGroup();
+
+		option1.setToggleGroup(group);
+		option2.setToggleGroup(group);
+		option3.setToggleGroup(group);
+		option4.setToggleGroup(group);
+
+		option1.setSelected(false);
+		option2.setSelected(false);
+		option3.setSelected(false);
+		option4.setSelected(false);
+
+		option1.setText(options.get(0));
+		option2.setText(options.get(1));
+		option3.setText(options.get(2));
+		option4.setText(options.get(3));
+	}
 
 }
-
--- student/src/controller/StudentController.java
+++ student/src/controller/StudentController.java
@@ -14,54 +14,51 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
 /**
  * Class to display the list of Quiz to take
  */
 public class StudentController implements Initializable {
 
-    @FXML
-    private ListView<String> quizList;
+	@FXML
+	private ListView<String> quizList;
 
-    private ObservableList quizNames =  FXCollections.observableArrayList();
+	private ObservableList quizNames = FXCollections.observableArrayList();
 
-    ShowQuiz questionAnswerObj = new ShowQuiz();
+	ShowQuiz questionAnswerObj;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-       getQuizNames(); listViewEventListener();
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		getQuizNames();
+		listViewEventListener();
+	}
 
-    public void getQuizNames(){
-        quizNames.add("Quiz1");
-        quizNames.add("Quiz2");
-        quizNames.add("Quiz3");
-        quizList.getItems().addAll(quizNames);
-    }
+	public void getQuizNames() {
+		quizNames.add("Quiz1");
+		quizNames.add("Quiz2");
+		quizNames.add("Quiz3");
+		quizList.getItems().addAll(quizNames);
+	}
 
-    public FXMLLoader loader;
+	private void setQuizTitle(String selectedQuiz) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/showQuiz.fxml"));
+		Parent root = loader.load();
+		questionAnswerObj = loader.getController();
+		questionAnswerObj.setQuizTitleLabel(selectedQuiz);
+		questionAnswerObj.fetchQuizDetails(selectedQuiz);
+		quizList.getScene().setRoot(root);
+	}
 
-    private void setQuizTitle(String selectedQuiz) throws IOException {
-        loader = new FXMLLoader(getClass().getResource("../view/showQuiz.fxml"));
-        Parent root = loader.load();
-        ShowQuiz quizController = loader.getController();
-        quizController.setQuizTitleLabel(selectedQuiz);
-        quizList.getScene().setRoot(root);
-    }
-
-    public void listViewEventListener() {
-        quizList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String selectedQuiz) {
-                try {
-                    setQuizTitle(selectedQuiz);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                ShowQuiz quizController = loader.getController();
-                quizController.fetchQuizDetails(selectedQuiz);
-            }
-        });
-    }
+	public void listViewEventListener() {
+		quizList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String selectedQuiz) {
+				try {
+					setQuizTitle(selectedQuiz);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 }
